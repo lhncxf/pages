1d728b7c55e8f20a054e5c068a2def457a30bfe0
luhaonan <kobeluhaonan@163.com>
2022-08-20 11:39:58 +0800

    update docs

--- update.sh
+++ update.sh
@@ -4,4 +4,10 @@ git add .
 
 git commit -m"update docs"
 
-git push -f origin gh-pages
+git push origin gh-pages
+
+git remote set-url origin git@gitee.com:lhncxf/Notes.git
+
+git push origin gh-pages
+
+git remote set-url origin git@github.com:lhncxf/pages.git
