f5b44e793c4762a71dbd28c834bedb11a54598d3
luhaonan <kobeluhaonan@163.com>
2022-08-20 11:43:47 +0800

    update docs

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ git commit -m"update docs"
 
 git push origin gh-pages
 
-git remote set-url origin git@gitee.com:lhncxf/Notes.git
+git remote set-url origin git@github.com:lhncxf/pages.git
 
 git push origin gh-pages
 
-git remote set-url origin git@github.com:lhncxf/pages.git
+git remote set-url origin git@gitee.com:lhncxf/Notes.git
