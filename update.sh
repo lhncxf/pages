fdb8e353c34d921741ebf28bd7d614158fedca49
luhaonan <kobeluhaonan@163.com>
2022-08-20 11:34:49 +0800

    update notes

---
+++ update.sh
@@ -0,0 +1,13 @@
+set -e
+
+git add .
+
+git commit -m"update notes"
+
+git push origin gh-pages
+
+git remote set-url origin git@gitee.com:lhncxf/Notes.git
+
+git push origin gh-pages
+
+git remote set-url origin git@github.com:lhncxf/pages.git
