63af6dd537ef8152dca683b78dcc9f5c351323d2
luhaonan <kobeluhaonan@163.com>
2022-08-24 10:54:17 +0800

    update docs

---
+++ _layouts/index.js
@@ -0,0 +1,20 @@
+function toggleClass(element, className) {
+    if (!element || !className) {
+        return
+    }
+    let classString = element.className
+    const nameIndex = classString.indexOf(className)
+    if (nameIndex === -1) {
+        classString += '' + className
+    } else {
+        classString =
+            classString.substr(0, nameIndex) +
+            classString.substr(nameIndex + className.length)
+    }
+    element.className = classString
+}
+
+
+document.querySelector('.preview-title').addEventListener('click', function () {
+    toggleClass(document.getElementById('previewContent'), 'hide')
+}, false)
